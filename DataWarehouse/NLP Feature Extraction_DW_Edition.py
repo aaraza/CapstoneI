0e9bcdb747589fcba8c54d8a02685f9b4ae98976
JustinRenneke <rennekej@gmail.com>
2017-10-31 18:31:18 -0500

    update data warehouse for sentiment analysis

--- DataWarehouse/NLP Feature Extraction_DW_Edition.py
+++ DataWarehouse/NLP Feature Extraction_DW_Edition.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
 import psycopg2
 import pandas as pd
 import nltk
@@ -12,14 +7,8 @@ import re
 from __future__ import division
 from nltk.tag import StanfordNERTagger
 
-
-# In[2]:
-
 conn = psycopg2.connect("dbname='cap' user='postgres' host='ec2-35-163-99-253.us-west-2.compute.amazonaws.com' port=9000 password ='secret'")
 df = pd.read_sql_query("SELECT * FROM articles limit 5", conn)
-
-
-# In[3]:
 
 tokenized_body = []
 for body in df['body']:
@@ -27,52 +16,27 @@ for body in df['body']:
     tokens = nltk.word_tokenize(body)
     tokenized_body.append(tokens)
 
-
-# In[4]:
-
 se = pd.Series(tokenized_body)
 df['tokenized_body'] = se.values
-
-
-# In[19]:
 
 word_count = []
 for body in df['tokenized_body']:
     word_count.append(len(body))
 
-
-# In[20]:
-
 se = pd.Series(word_count)
 df['word_count'] = se.values
-
-
-# In[21]:
 
 stop_words = stopwords.words('english')
 stop_words = stop_words + [',', '.', '!', '?', '"','\'', '/', '\\', '-', '--', '—', '(', ')', '[', ']', '\'s', '\'t', '\'ve', '\'d', '\'ll', '\'re']
 stop_words = set(stop_words) # making this a set increases performance for large documents
 
 
-# In[34]:
-
-df
-
-
-# In[22]:
-
 stopworded_body = []
 for body in df['tokenized_body']:
     stopworded_body.append([w.lower() for w in body if w not in stop_words])
 
-
-# In[23]:
-
 se = pd.Series(stopworded_body)
 df['stopworded_body'] = se.values
-
-
-# In[24]:
 
 wnl = nltk.WordNetLemmatizer()
 lemmatized_words = []
@@ -91,55 +55,26 @@ for body in df['stopworded_body']:
         lemmatized_words.append(lemma)
     lemmatized_body.append(lemmatized_words)
 
-
-# In[25]:
-
 se = pd.Series(lemmatized_body)
 df['lemmatized_body'] = se.values
-
-
-# In[26]:
 
 word_bag = []
 for body in df['lemmatized_body']:
     fdist = FreqDist(body)
     # FreqDist returns a special nltk.probability.FreqDist type
     # This is a list of tuples
-    # Here is an example of how to access the elements for future reference
-#     print(fdist.most_common())
-    # Access an individual tuple
-#     print(fdist.most_common()[0])
-    # Access the word from the tuple
-#     print(fdist.most_common()[0][0])
-    # Access the count from the tuple
-#     print(fdist.most_common()[0][1])
-    # Append to list as ordered frequency distribution
     word_bag.append(fdist.most_common())
-
-
-# In[27]:
 
 se = pd.Series(word_bag)
 df['word_bag'] = se.values
-
-
-# In[28]:
 
 st = StanfordNERTagger('/media/justin/Data/Google Drive/Assignments and Projects/Machine Learning/NLP/english.all.3class.distsim.crf.ser.gz',
 					   '/media/justin/Data/Google Drive/Assignments and Projects/Machine Learning/NLP/stanford-ner.jar',
 					   encoding='utf-8')
 
-
-# In[29]:
-
 classified_texts = []
 for body in df['tokenized_body']:
     classified_texts.append(st.tag(body))
-
-# print(classified_text)
-
-
-# In[30]:
 
 from nltk import pos_tag
 from nltk.chunk import conlltags2tree
@@ -165,9 +100,6 @@ def stanfordNE2BIO(tagged_sent):
 
     return bio_tagged_sent
 
-
-# In[31]:
-
 def stanfordNE2tree(ne_tagged_sent):
     bio_tagged_sent = stanfordNE2BIO(ne_tagged_sent)
     sent_tokens, sent_ne_tags = zip(*bio_tagged_sent)
@@ -177,18 +109,12 @@ def stanfordNE2tree(ne_tagged_sent):
     ne_tree = conlltags2tree(sent_conlltags)
     return ne_tree
 
-
-# In[40]:
-
 ne_trees = []
 for text in classified_texts:
     try:
         ne_trees.append(stanfordNE2tree(text))
     except:
         ne_trees.append(' ')
-
-
-# In[41]:
 
 ne_in_sent = []
 ne_in_sents = []
@@ -201,38 +127,21 @@ for tree in ne_trees:
             ne_in_sent.append((ne_string, ne_label))
     ne_in_sents.append(ne_in_sent)
 
-
-# In[42]:
-
 se = pd.Series(ne_in_sents)
 df['named_entities'] = se.values
 
-
-# In[43]:
-
 def lexical_diversity(text):
     return len(set(text)) / len(text) * 100
-
-
-# In[44]:
 
 lex_div = []
 for body in df['stopworded_body']:
     lex_div.append(lexical_diversity(body))
 
-
-# In[45]:
-
 se = pd.Series(lex_div)
 df['lexical_diversity'] = se.values
 
+#get_ipython().system(u'jupyter nbconvert --to script /home/justin/GitHub/CapstoneI/config_template.ipynb')
 
-# In[48]:
-
-get_ipython().system(u'jupyter nbconvert --to script /home/justin/GitHub/CapstoneI/config_template.ipynb')
-
-
-# In[ ]:
 
 
 
